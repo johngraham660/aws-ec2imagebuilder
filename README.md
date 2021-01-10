6b96dd7870d2309686db23b7f06bf8b159f3f12f
John Graham <johngraham660@gmail.com>
2021-01-10 15:38:43 +0000

    Building repo

---
+++ README.md
@@ -0,0 +1,6 @@
+# AWS EC2ImageBuilder Module
+
+![Terraform Version](https://img.shields.io/badge/tf-%3E%3D0.12.0-blue.svg)
+
+This repo contains a set of modules for deploying EC2 ImageBuilder components to automate the creation of custom/standardised Amazon Machine Images (AMI's)
+
---
+++ main.tf
@@ -0,0 +1,12 @@
+# ----------------------------------------------------------------------------------------------------
+# DEPLOY AN EC2IMAGEBUILDER PIPELINE IN AWS
+# This will deploy an EC2ImageBuilder pipeline in AWS that creates a standardised Amazon Machine Image
+# ----------------------------------------------------------------------------------------------------
+
+terraform {
+  required_version = ">= 0.12.26"
+}
+
+
+module ":wq!
+
