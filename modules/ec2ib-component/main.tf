19b84e5f477190e82559a78f8455d8d46f5743d0
John Graham <johngraham660@gmail.com>
2021-01-10 20:23:17 +0000

    Deletes all modules in main repo, these have moved to dedicated repositories

--- modules/ec2ib-component/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_imagebuilder_component" "build_componment" {
-  data                  = var.data
-  name                  = var.name
-  platform              = var.platform
-  version               = var.version
-  description           = var.description
-  supported_os_versions = [ var.supported_os_versions ]
-}
